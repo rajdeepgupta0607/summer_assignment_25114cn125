e6ba2b5a6bb1c8e0a949ad3ef29cf9512fad0fec
Raj deep Gupta <rajdeepgupta2007@gmail.com>
2026-06-02 13:03:56 +0530

    day 1

---
+++ day_1/_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    
+    if (n < 1) {
+        printf("Please enter a natural number.\n");
+    } else {
+        // Apply the formula
+        sum = n * (n + 1) / 2;
+        printf("The sum of first %d natural numbers is: %d\n", n, sum);
+    }
+    
+    return 0;
+}
